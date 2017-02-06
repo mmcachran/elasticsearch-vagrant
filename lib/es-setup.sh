0421064ee11c06c439cb9ff1eedecc19c1fc5e7d
Matt McAchran <matt.mcachran@webdevstudios.com>
2017-02-06 16:20:33 -0500

    Add vim and htop, install ES plugins, cleanup.

--- lib/es-setup.sh
+++ lib/es-setup.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
+# Install TMUX and start a new TMUX session.
+apt-get install tmux
+tmux new -s elastic
+
 # Setting ES version to install.
 ES_VERSION="2.4.1"
-ES_PLUGIN_INSTALL_CMD="/usr/share/elasticsearch/bin/plugin -install"
+ES_PLUGIN_INSTALL_CMD="/usr/share/elasticsearch/bin/plugin install "
 
 apt-get -yq update
 apt-get -yq install git openjdk-7-jre-headless service-wrapper curl
 
 update-alternatives --set java /usr/lib/jvm/java-7-openjdk-i386/jre/bin/java
 
-# Install TMUX
-sudo apt-get install tmux
-
 # Install VIM.
-sudo apt-get install vim
+apt-get install vim
+
+# Install HTOP.
+apt-get install htop
 
 # Install ElasticSearch.
 wget -nv https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/$ES_VERSION/elasticsearch-$ES_VERSION.deb
@@ -37,9 +41,10 @@ ES_PLUGINS+=(lukas-vlcek/bigdesk)
 ES_PLUGINS+=(elasticsearch/elasticsearch-analysis-icu/2.4.1)
 ES_PLUGINS+=(lmenezes/elasticsearch-kopf)
 ES_PLUGINS+=(royrusso/elasticsearch-HQ)
+ES_PLUGINS+=(elasticsearch/marvel/latest)
 
 # Loop through and install ES plugins.
-for P in ${ESPLUGINS[*]}
+for P in ${ES_PLUGINS[*]}
 do
 	${ES_PLUGIN_INSTALL_CMD} $P
 done
---
+++ lib/inline-script.rb
@@ -0,0 +1,3 @@
+@node_start_inline_script = <<SCRIPT
+
+SCRIPT
