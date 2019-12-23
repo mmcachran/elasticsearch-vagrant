676a3ba0cac661bba1eff16a43e7d8fda235ef24
mmcachran <mattmcachran@gmail.com>
2019-12-23 11:20:34 -0500

    Cleanup.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ElasticSearch Vagrant Box
 
-Requirements:
+## Elasticsearch
+*Version:* 7.5.1
 
+Requirements:
 * https://www.vagrantup.com/
 * https://www.virtualbox.org/
 
@@ -9,18 +11,3 @@ Once those are installed:
 * Clone this repository
 * `vagrant up` in the root of this repository
 * Visit `http://10.0.0.11:9200` in your browser
-
-## Elasticsearch
-*Version:* 7.5.1
-
-## Included plugins
-
-This configuration includes the following plugins:
-
-* Bigdesk: http://bigdesk.org/
-* HQ: http://www.elastichq.org/
-* HEAD: https://github.com/mobz/elasticsearch-head
-* Image: https://github.com/kzwang/elasticsearch-image
-* Mapper Attachments: https://github.com/elasticsearch/elasticsearch-mapper-attachments
-* ICU Analysis: https://github.com/elasticsearch/elasticsearch-analysis-icu
-* Kopf: https://github.com/lmenezes/elasticsearch-kopf
--- lib/es-setup.sh
+++ lib/es-setup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Setting ES version to install.
-ES_VERSION="7.4.2"
-ES_PLUGIN_INSTALL_CMD="/usr/share/elasticsearch/bin/plugin install "
 
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
@@ -18,10 +15,6 @@ sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.5.list
 sudo apt-get update && sudo apt-get install elasticsearch
 
-
-# wget -nv "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-amd64.deb"
-# sudo dpkg -i "elasticsearch-$ES_VERSION-amd64.deb"
-
 chmod g+ws /etc/elasticsearch/
 mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.bak
 cp /home/vagrant/config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
@@ -29,26 +22,8 @@ cp /home/vagrant/config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 # update-rc.d elasticsearch defaults 95 10
 /etc/init.d/elasticsearch start
 
-# Holds ES plugins we want to install.
-ES_PLUGINS=()
-
-# Internal Plugins.
-# ES_PLUGINS+=(com.github.kzwang/elasticsearch-image/1.2.0)
-# ES_PLUGINS+=(elasticsearch/elasticsearch-mapper-attachments/2.4.2)
-
-# Supervision/Dashboard ES plugins.
-# ES_PLUGINS+=(mobz/elasticsearch-head)
-# ES_PLUGINS+=(lukas-vlcek/bigdesk)
-# ES_PLUGINS+=(elasticsearch/elasticsearch-analysis-icu/2.4.1)
-# ES_PLUGINS+=(lmenezes/elasticsearch-kopf)
-# ES_PLUGINS+=(royrusso/elasticsearch-HQ)
-# ES_PLUGINS+=(elasticsearch/marvel/latest)
-
-# Loop through and install ES plugins.
-for P in ${ES_PLUGINS[*]}
-do
-	${ES_PLUGIN_INSTALL_CMD} $P
-done
+# Install Kibana.
+# sudo apt-get update && sudo apt-get install kibana
 
 /etc/init.d/elasticsearch restart
 # or service elasticsearch restart
